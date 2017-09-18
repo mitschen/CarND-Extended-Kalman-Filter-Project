f196fa94d0b63977394fe703eb2f15d738200be5
mitschen <mitschen@gmail.com>
2017-09-18 17:35:07 +0200

    Added function for rmse calculation

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,6 +15,21 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+  VectorXd RMSE_return(4);
+  int const c_maxCnt(estimations.size()>ground_truth.size()?
+        estimations.size():ground_truth.size());
+
+  //calculate the sum of the squares
+  for(int idx(0); idx < c_maxCnt; idx++)
+  {
+    VectorXd square_balance(estimations[idx]-ground_truth[idx]);
+    square_balance = square_balance.array()*square_balance.array();
+    RMSE_return = RMSE_return + square_balance;
+  }
+  //devide the sum with the number of elements
+  RMSE_return = RMSE_return / c_maxCnt;
+  //return the sqare-root
+  return RMSE_return.array().sqrt();
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
